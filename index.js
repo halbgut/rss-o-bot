21eb737c2ba08f993ce4bda19b9a3f07c4e2c5a5
Kriegslustig <kriegslustig@kriegslustig.me>
2016-06-03 10:06:01 +0200

    Get a first working version

--- index.js
+++ index.js
@@ -5,24 +5,28 @@ const notify = require('./lib/notify')(config)
 const poll = require('./lib/poll.js')
 const initStore = require('./lib/store.js')
 
-Rx.Observable.combineLatest(
-  initStore(config),
-  Rx.Observable.interval(config.interval * 1000),
-  ({getFeeds, insertFeed, updateLatestLink}) => {
+Rx.Observable.interval(config.interval * 1000).startWith(0).flatMap(
+  initStore(config).flatMap(({getFeeds, insertFeed, updateLatestLink}) =>
     getFeeds()
-      .flatMap(feeds => Rx.combineLatest(
+      .flatMap((feeds) => Rx.Observable.combineLatest(
         ...feeds.map(feed =>
           poll(feed.get('url'))
             .filter(({latestLink}) =>
               latestLink !== feed.get('latestLink')
             )
-            .map(({ blog, link }) => notify(blog, link))
+            .flatMap(({ blog, latestLink }) =>
+              Rx.Observable.forkJoin(
+                notify(blog, latestLink),
+                updateLatestLink(feed.get('id'), latestLink)
+              )
+            )
         )
       ))
-  }
+  )
 )
   .subscribe(
     console.log,
-    console.error
+    console.error,
+    () => console.log('Complete')
   )
 
