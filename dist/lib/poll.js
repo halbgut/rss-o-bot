2f126124c3bfa2ec51b26b36755aa828e817f9da
Kriegslustig <kriegslustig@kriegslustig.me>
2016-06-07 18:35:18 +0200

    Replace the bloated request library

--- dist/lib/poll.js
+++ dist/lib/poll.js
@@ -2,25 +2,73 @@
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
-var request = require('request');
+var http = require('http');
+var https = require('https');
+var urlUtil = require('url');
+
 var Feedparser = require('feedparser');
 var Rx = require('rx');
+var O = Rx.Observable;
+
+var isRedirect = function isRedirect(res) {
+  return res.statusCode >= 300 && res.statusCode <= 399 && res.headers.location;
+};
+var isSuccessful = function isSuccessful(res) {
+  return res.statusCode >= 200 && res.statusCode < 400;
+};
+
+var get = function get(url) {
+  var depth = arguments.length <= 1 || arguments[1] === undefined ? 0 : arguments[1];
+  return O.create(function (o) {
+    var _urlUtil$parse = urlUtil.parse(url);
+
+    var host = _urlUtil$parse.host;
+    var path = _urlUtil$parse.path;
+    var protocol = _urlUtil$parse.protocol;
+
+    var request = protocol === 'http:' ? http.request : https.request;
+    request({
+      host: host, path: path,
+      headers: { 'User-Agent': 'RSS-o-Bot' }
+    }, function (res) {
+      console.log(res.statusCode);
+      var body = '';
+      if (isRedirect(res)) {
+        if (depth > 10) {
+          // maximum redirects
+          return o.onError(new Error('Maximum redirects reached'));
+        }
+        get(res.headers.location, ++depth).subscribe(function (v) {
+          return o.onNext(v);
+        }, function (err) {
+          return o.onError(err);
+        }, function (v) {
+          return o.onCompleted();
+        });
+      } else if (isSuccessful(res)) {
+        res.on('data', function (chunk) {
+          body += chunk;
+        });
+        res.on('end', function () {
+          o.onNext(body);o.onCompleted();
+        });
+        res.on('error', function (err) {
+          return o.onError(err);
+        });
+      } else {
+        o.onError(new Error('Request failed: ' + url));
+      }
+    }).on('error', function (err) {
+      return o.onError(err);
+    }).end();
+  });
+};
 
 var _require = require('./helpers');
 
 var isAbsoluteUrl = _require.isAbsoluteUrl;
 var getBaseUrl = _require.getBaseUrl;
 
-
-var get = function get(url) {
-  return Rx.Observable.create(function (o) {
-    request(url, function (err, res, body) {
-      if (err || res.statusCode !== 200) o.onError(err || res);
-      o.onNext(body);
-      o.onCompleted();
-    });
-  });
-};
 
 function parse(xml) {
   return Rx.Observable.create(function (o) {
--- src/lib/poll.js
+++ src/lib/poll.js
@@ -1,16 +1,49 @@
-const request = require('request')
+const http = require('http')
+const https = require('https')
+const urlUtil = require('url')
+
 const Feedparser = require('feedparser')
 const Rx = require('rx')
+const O = Rx.Observable
+
+const isRedirect = res => res.statusCode >= 300 && res.statusCode <= 399 && res.headers.location
+const isSuccessful = res => res.statusCode >= 200 && res.statusCode < 400
+
+const get = (url, depth = 0) => O.create(o => {
+  const {host, path, protocol} = urlUtil.parse(url)
+  const request =
+    protocol === 'http:'
+      ? http.request
+      : https.request
+  request({
+    host, path,
+    headers: { 'User-Agent': 'RSS-o-Bot' }
+  }, res => {
+    console.log(res.statusCode)
+    let body = ''
+    if (isRedirect(res)) {
+      if (depth > 10) { // maximum redirects
+        return o.onError(new Error('Maximum redirects reached'))
+      }
+      get(res.headers.location, ++depth)
+        .subscribe(
+          v => o.onNext(v),
+          err => o.onError(err),
+          v => o.onCompleted()
+        )
+    } else if (isSuccessful(res)) {
+      res.on('data', chunk => { body += chunk })
+      res.on('end', () => { o.onNext(body); o.onCompleted() })
+      res.on('error', err => o.onError(err))
+    } else {
+      o.onError(new Error(`Request failed: ${url}`))
+    }
+  })
+    .on('error', err => o.onError(err))
+    .end()
+})
 
 const {isAbsoluteUrl, getBaseUrl} = require('./helpers')
-
-const get = url => Rx.Observable.create(o => {
-  request(url, (err, res, body) => {
-    if (err || res.statusCode !== 200) o.onError(err || res)
-    o.onNext(body)
-    o.onCompleted()
-  })
-})
 
 function parse (xml) {
   return Rx.Observable.create(o => {
