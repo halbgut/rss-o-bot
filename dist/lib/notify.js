c4502efa48b3ff98cda9db5b631bf7a4cf8f5053
Kriegslustig <kriegslustig@kriegslustig.me>
2016-06-10 16:23:53 +0200

    Pass just the title to notifiers and add some comments

--- dist/lib/notify.js
+++ dist/lib/notify.js
@@ -4,29 +4,40 @@ var Rx = require('rx');
 var debug = require('debug')('rss-o-bot');
 
 module.exports = function (config) {
-  var sends = config['notification-methods'].map(function (m) {
+  var sends =
+  /**
+   * Map over all configured notification methods and check if there
+   * are installed modules by that name.
+   */
+  config['notification-methods'].map(function (m) {
     var module = 'rss-o-bot-' + m;
     var msg = 'Successfully loaded notifier: ' + module;
     try {
       try {
+        /* Attempt local require */
         var send = require(module)(config);
         debug(msg);
         return send;
       } catch (e) {
-        var _send = require(__dirname + '/../../../' + module)(config); // Global require
+        /* Attempt global require */
+        var _send = require(__dirname + '/../../../' + module)(config);
         debug(msg);
         return _send;
       }
     } catch (e) {
+      /* Notifier not found */
       console.error(e);
       debug('Failed to load notifier: ' + module);
     }
   }).filter(function (f) {
     return f;
-  });
+  }); /* Exclude all notifiers, that weren't found */
   return function (blog, link, title) {
-    return Rx.Observable.forkJoin(sends.map(function (f) {
-      return f(blog + ' posted something new.', link, title);
-    }));
+    return(
+      /* Call all registered notifiers */
+      Rx.Observable.forkJoin(sends.map(function (f) {
+        return f(blog, link, title);
+      }))
+    );
   };
 };
--- src/lib/notify.js
+++ src/lib/notify.js
@@ -3,27 +3,35 @@ const debug = require('debug')('rss-o-bot')
 
 module.exports = (config) => {
   const sends =
+    /**
+     * Map over all configured notification methods and check if there
+     * are installed modules by that name.
+     */
     config['notification-methods'].map(m => {
       const module = `rss-o-bot-${m}`
       const msg = `Successfully loaded notifier: ${module}`
       try {
         try {
+         /* Attempt local require */
           const send = require(module)(config)
           debug(msg)
           return send
         } catch (e) {
-          const send = require(`${__dirname}/../../../${module}`)(config) // Global require
+         /* Attempt global require */
+          const send = require(`${__dirname}/../../../${module}`)(config)
           debug(msg)
           return send
         }
       } catch (e) {
+        /* Notifier not found */
         console.error(e)
         debug(`Failed to load notifier: ${module}`)
       }
-    }).filter(f => f)
+    }).filter(f => f) /* Exclude all notifiers, that weren't found */
   return (blog, link, title) =>
+    /* Call all registered notifiers */
     Rx.Observable.forkJoin(
-      sends.map(f => f(`${blog} posted something new.`, link, title))
+      sends.map(f => f(blog, link, title))
     )
 }
 
