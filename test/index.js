c63c508c72db43d92146d258ebaf998445634af9
Kriegslustig <kriegslustig@kriegslustig.me>
2016-07-03 18:11:41 +0200

    Add tests for -m and -h

--- test/index.js
+++ test/index.js
@@ -2,17 +2,39 @@ const {test} = require('ava')
 const runCLI = require('../src/cli.js')
 const configLocations = [`${__dirname}/config/local`]
 
-const run = t => n => o => {
+const run = (a, n = 1) => f => t => {
   t.plan(n)
-  o.subscribe(() => {}, console.error, () => t.end())
+  const o = runCLI(['node', '', a], configLocations)
+  f(t, o)
+    .catch(e => t.fail(e))
+    .subscribe(
+      () => {},
+      console.error,
+      () => t.end()
+    )
 }
 
-test.cb('version', t => run(t)(1)(
-  runCLI(['node', '', '-v'], configLocations)
-    .map(version =>
-      version.match(/RSS\-o\-Bot Version: \d+\.\d+\.\d+/)
-        ? t.pass()
-        : t.fail()
-    )
+test.cb('version', run('-v')((t, o) =>
+  o.map(version =>
+    version.match(/RSS\-o\-Bot Version: \d+\.\d+\.\d+/)
+      ? t.pass()
+      : t.fail()
+  )
+))
+
+test.cb('help', run('-h')((t, o) =>
+  o.map(help =>
+    help.length > 100
+      ? t.pass()
+      : t.fail()
+  )
+))
+
+test.cb('man', run('-m')((t, o) =>
+  o.map(man =>
+    man.length > 1000
+      ? t.pass()
+      : t.fail()
+  )
 ))
 
