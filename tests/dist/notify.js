adf8d26bf4b35fdd238360c2dba57e7ce56ffc68
Kriegslustig <kriegslustig@kriegslustig.me>
2016-08-08 17:25:04 +0200

    Implement test for multiple new posts

--- tests/dist/notify.js
+++ tests/dist/notify.js
@@ -2,6 +2,10 @@
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
 
+/* Mainly scenario tests for the notify module.
+ * But it kindof flows into some tests for the pollFeeds module.
+ */
+
 var _require = require('ava');
 
 var test = _require.test;
@@ -19,19 +23,16 @@ var Poll = require('../../dist/lib/pollFeeds/lib/poll.js')(H);
 var T = require('./lib/helpers')({ runCLI: runCLI, initStore: initStore, Config: Config });
 
 var createDummyEntryAndPoll = function createDummyEntryAndPoll(config, url) {
+  var offset = arguments.length <= 2 || arguments[2] === undefined ? 2 : arguments[2];
   return T.createDummyEntry(url, [], config, true).flatMap(function (_ref) {
     var _ref2 = _slicedToArray(_ref, 2);
 
     var store = _ref2[0];
     var feed = _ref2[1];
     return Poll(url, []).map(function (entries) {
-      return entries.slice(0, 2);
-    }).flatMap(function (_ref3) {
-      var _ref4 = _slicedToArray(_ref3, 2);
-
-      var latest = _ref4[0];
-      var link = _ref4[1].link;
-      return store.updateLatestLink(feed.get('id'), link).map(latest);
+      return entries.slice(0, offset);
+    }).flatMap(function (entries) {
+      return store.updateLatestLink(feed.get('id'), entries[offset - 1].link).map(entries.slice(0, -1));
     });
   });
 };
@@ -60,8 +61,32 @@ test.cb('notifiers/poll-feeds order', function (t) {
   var config = T.getConfigWithDefaults({
     'notification-methods': [notify]
   });
-  createDummyEntryAndPoll(config, url).tap(function (latest) {
+  createDummyEntryAndPoll(config, url).tap(function (_ref3) {
+    var _ref4 = _slicedToArray(_ref3, 1);
+
+    var latest = _ref4[0];
     latestItem = latest;
+  }).flatMap(function () {
+    return runCLI(['node', '', 'poll-feeds'], null, config);
+  }).subscribe(function () {}, T.handleError(t));
+});
+
+test.cb('poll-feeds multiple new posts', function (t) {
+  var latestItems = void 0;
+  var i = 0;
+  var notify = function notify(config) {
+    return function (blog, link, title) {
+      t.is(link, latestItems[i].link);
+      if (++i === 1) t.end();
+      return O.of(true);
+    };
+  };
+  var url = 'https://lucaschmid.net/feed/rss.xml';
+  var config = T.getConfigWithDefaults({
+    'notification-methods': [notify]
+  });
+  createDummyEntryAndPoll(config, url, 3).tap(function (latest) {
+    latestItems = latest.reverse();
   }).flatMap(function () {
     return runCLI(['node', '', 'poll-feeds'], null, config);
   }).subscribe(function () {}, T.handleError(t));
--- tests/src/notify.js
+++ tests/src/notify.js
@@ -1,3 +1,7 @@
+/* Mainly scenario tests for the notify module.
+ * But it kindof flows into some tests for the pollFeeds module.
+ */
+
 const { test } = require('ava')
 const { Observable: O } = require('rx')
 
@@ -8,13 +12,14 @@ const Config = require('../../dist/lib/config')(H)
 const Poll = require('../../dist/lib/pollFeeds/lib/poll.js')(H)
 const T = require('./lib/helpers')({ runCLI, initStore, Config })
 
-const createDummyEntryAndPoll = (config, url) =>
+const createDummyEntryAndPoll = (config, url, offset = 2) =>
   T.createDummyEntry(url, [], config, true)
     .flatMap(([store, feed]) =>
       Poll(url, [])
-        .map(entries => entries.slice(0, 2))
-        .flatMap(([latest, { link }]) =>
-          store.updateLatestLink(feed.get('id'), link).map(latest)
+        .map(entries => entries.slice(0, offset))
+        .flatMap(entries =>
+          store.updateLatestLink(feed.get('id'), entries[offset - 1].link)
+            .map(entries.slice(0, -1))
         )
     )
 
@@ -44,7 +49,28 @@ test.cb('notifiers/poll-feeds order', t => {
     'notification-methods': [notify]
   })
   createDummyEntryAndPoll(config, url)
-    .tap(latest => { latestItem = latest })
+    .tap(([latest]) => { latestItem = latest })
+    .flatMap(() => runCLI(['node', '', 'poll-feeds'], null, config))
+    .subscribe(
+      () => {},
+      T.handleError(t)
+    )
+})
+
+test.cb('poll-feeds multiple new posts', t => {
+  let latestItems
+  let i = 0
+  const notify = config => (blog, link, title) => {
+    t.is(link, latestItems[i].link)
+    if (++i === 1) t.end()
+    return O.of(true)
+  }
+  const url = 'https://lucaschmid.net/feed/rss.xml'
+  const config = T.getConfigWithDefaults({
+    'notification-methods': [notify]
+  })
+  createDummyEntryAndPoll(config, url, 3)
+    .tap(latest => { latestItems = latest.reverse() })
     .flatMap(() => runCLI(['node', '', 'poll-feeds'], null, config))
     .subscribe(
       () => {},
