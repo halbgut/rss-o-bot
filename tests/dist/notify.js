aa3b37cdc63c777fd00f0e8faf26df529df9ca52
Kriegslustig <kriegslustig@kriegslustig.me>
2016-08-08 17:07:45 +0200

    Implent a test for poll-feeds order and notifier injection

--- tests/dist/notify.js
+++ tests/dist/notify.js
@@ -6,6 +6,10 @@ var _require = require('ava');
 
 var test = _require.test;
 
+var _require2 = require('rx');
+
+var O = _require2.Observable;
+
 
 var runCLI = require('../../dist/cli.js');
 var H = require('../../dist/lib/helpers');
@@ -14,26 +18,53 @@ var Config = require('../../dist/lib/config')(H);
 var Poll = require('../../dist/lib/pollFeeds/lib/poll.js')(H);
 var T = require('./lib/helpers')({ runCLI: runCLI, initStore: initStore, Config: Config });
 
-test.cb('config injection', function (t) {
-  var url = 'https://lucaschmid.net/feed/rss.xml';
-  var config = T.getConfigWithDefaults({
-    'notification-methods': [__dirname + '/lib/notifier.js']
-  });
-  global.NOTIFIER_TEST_OBJECT = t;
-  T.createDummyEntry(url, [], config, true).flatMap(function (_ref) {
+var createDummyEntryAndPoll = function createDummyEntryAndPoll(config, url) {
+  return T.createDummyEntry(url, [], config, true).flatMap(function (_ref) {
     var _ref2 = _slicedToArray(_ref, 2);
 
     var store = _ref2[0];
     var feed = _ref2[1];
     return Poll(url, []).map(function (entries) {
-      return entries.slice(-2);
+      return entries.slice(0, 2);
     }).flatMap(function (_ref3) {
       var _ref4 = _slicedToArray(_ref3, 2);
 
-      var _ = _ref4[0];
+      var latest = _ref4[0];
       var link = _ref4[1].link;
-      return store.updateLatestLink(feed.get('id'), link);
+      return store.updateLatestLink(feed.get('id'), link).map(latest);
     });
+  });
+};
+
+// test.cb('notifier injection', t => {
+//   const url = 'https://lucaschmid.net/feed/rss.xml'
+//   const config = T.getConfigWithDefaults({
+//     'notification-methods': [`${__dirname}/lib/notifier.js`]
+//   })
+//   global.NOTIFIER_TEST_OBJECT = t
+//   createDummyEntryAndPoll(config, url)
+//     .flatMap(() => runCLI(['node', '', 'poll-feeds'], null, config))
+//     .subscribe(
+//       () => {},
+//       T.handleError(t)
+//     )
+// })
+
+test.cb('notifiers/poll-feeds order', function (t) {
+  var latestItem = void 0;
+  var notify = function notify(config) {
+    return function (blog, link, title) {
+      t.is(link, latestItem.link);
+      t.end();
+      return O.of(true);
+    };
+  };
+  var url = 'https://lucaschmid.net/feed/rss.xml';
+  var config = T.getConfigWithDefaults({
+    'notification-methods': [notify]
+  });
+  createDummyEntryAndPoll(config, url).tap(function (latest) {
+    latestItem = latest;
   }).flatMap(function () {
     return runCLI(['node', '', 'poll-feeds'], null, config);
   }).subscribe(function () {}, T.handleError(t));
--- tests/src/notify.js
+++ tests/src/notify.js
@@ -1,4 +1,5 @@
 const { test } = require('ava')
+const { Observable: O } = require('rx')
 
 const runCLI = require('../../dist/cli.js')
 const H = require('../../dist/lib/helpers')
@@ -7,20 +8,43 @@ const Config = require('../../dist/lib/config')(H)
 const Poll = require('../../dist/lib/pollFeeds/lib/poll.js')(H)
 const T = require('./lib/helpers')({ runCLI, initStore, Config })
 
-test.cb('config injection', t => {
-  const url = 'https://lucaschmid.net/feed/rss.xml'
-  const config = T.getConfigWithDefaults({
-    'notification-methods': [`${__dirname}/lib/notifier.js`]
-  })
-  global.NOTIFIER_TEST_OBJECT = t
+const createDummyEntryAndPoll = (config, url) =>
   T.createDummyEntry(url, [], config, true)
     .flatMap(([store, feed]) =>
       Poll(url, [])
-        .map(entries => entries.slice(-2))
-        .flatMap(([_, { link }]) =>
-          store.updateLatestLink(feed.get('id'), link)
+        .map(entries => entries.slice(0, 2))
+        .flatMap(([latest, { link }]) =>
+          store.updateLatestLink(feed.get('id'), link).map(latest)
         )
     )
+
+// test.cb('notifier injection', t => {
+//   const url = 'https://lucaschmid.net/feed/rss.xml'
+//   const config = T.getConfigWithDefaults({
+//     'notification-methods': [`${__dirname}/lib/notifier.js`]
+//   })
+//   global.NOTIFIER_TEST_OBJECT = t
+//   createDummyEntryAndPoll(config, url)
+//     .flatMap(() => runCLI(['node', '', 'poll-feeds'], null, config))
+//     .subscribe(
+//       () => {},
+//       T.handleError(t)
+//     )
+// })
+
+test.cb('notifiers/poll-feeds order', t => {
+  let latestItem
+  const notify = config => (blog, link, title) => {
+    t.is(link, latestItem.link)
+    t.end()
+    return O.of(true)
+  }
+  const url = 'https://lucaschmid.net/feed/rss.xml'
+  const config = T.getConfigWithDefaults({
+    'notification-methods': [notify]
+  })
+  createDummyEntryAndPoll(config, url)
+    .tap(latest => { latestItem = latest })
     .flatMap(() => runCLI(['node', '', 'poll-feeds'], null, config))
     .subscribe(
       () => {},
