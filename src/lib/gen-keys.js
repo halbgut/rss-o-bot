76ab1177d4e45f2e1c6c806438fffbfb407cc613
Kriegslustig <kriegslustig@kriegslustig.me>
2016-09-11 18:37:40 +0200

    Fix the genKeys openssl call

--- src/lib/gen-keys.js
+++ src/lib/gen-keys.js
@@ -10,15 +10,14 @@ const genKeys = (H, Errors) => config => {
   const privateKeyPath = H.privateKeyPath(config)
   const publicKeyPath = H.publicKeyPath(config)
   return (
-    H.exec('openssl genrsa 4096 -outform PEM')
+    H.exec(`openssl genrsa -out ${privateKeyPath} 4096`)
+      .delay(100)
       .do(() => debug('Tried to generate RSA private key'))
-      .flatMap(k => H.writeFile(privateKeyPath, k[0]))
       .flatMap(() => H.isFile(privateKeyPath).catch(err => {
         debug(err)
         return Errors.throwO(Errors.FAILED_TO_GEN_PRIV_KEY)
       }))
-      .flatMap(() => H.exec(`openssl rsa -pubout -in ${privateKeyPath}`))
-      .flatMap(k => H.writeFile(publicKeyPath, k[0]))
+      .flatMap(() => H.exec(`openssl rsa -pubout -in ${privateKeyPath} -out ${publicKeyPath}`))
       .flatMap(() => H.isFile(publicKeyPath).catch(err => {
         debug(err)
         return Errors.throwO(Errors.FAILED_TO_GEN_PUB_KEY)
