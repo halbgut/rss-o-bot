03e57d5d347a0f39fb9b5185cbf1973f1b9f3e82
Kriegslustig <kriegslustig@kriegslustig.me>
2016-09-11 16:52:44 +0200

    Improve genKeys

--- dist/lib/gen-keys.js
+++ dist/lib/gen-keys.js
@@ -2,40 +2,29 @@
 
 var debug = require('debug')('rss-o-bot');
 
-var _require = require('rx');
-
-var O = _require.Observable;
-
 /**
  * OS specific approach. Works on MacOS but probably not
  * on Windows. It probably also works on Linux. If this doesn't
  * work, the RSA keys must be generated manualy.
  */
-
 var genKeys = function genKeys(H, Errors) {
-  var i = 0;
-  var genKeysBound = function genKeysBound(config) {
+  return function (config) {
     var privateKeyPath = H.privateKeyPath(config);
     var publicKeyPath = H.publicKeyPath(config);
     return H.exec('openssl genrsa 4096 -outform PEM').do(function () {
       return debug('Tried to generate RSA private key');
+    }).flatMap(function (k) {
+      return H.writeFile(privateKeyPath, k);
     }).flatMap(H.isFile(privateKeyPath).catch(function () {
       return Errors.throwO(Errors.FAILED_TO_GEN_PRIV_KEY);
-    })).doOnError(function (err) {
-      return console.log(err);
-    }).flatMap(H.exec('openssl rsa -pubout -in ' + privateKeyPath)).flatMap(function (k) {
-      return console.log('writting to ' + privateKeyPath + ': ' + k) || H.writeFile(privateKeyPath, k);
-    }).flatMap(function (k) {
+    })).flatMap(H.exec('openssl rsa -pubout -in ' + privateKeyPath)).flatMap(function (k) {
       return H.writeFile(publicKeyPath, k);
-    }).flatMap(H.isFile(privateKeyPath).catch(function () {
+    }).flatMap(H.isFile(publicKeyPath).catch(function () {
       return Errors.throwO(Errors.FAILED_TO_GEN_PUB_KEY);
     })).do(function () {
       return debug('Generated keypair.');
-    }).catch(function (err) {
-      return ++i > 1 ? O.throw(err) : genKeysBound(config);
-    });
+    }).retry(2);
   };
-  return genKeysBound;
 };
 
 module.exports = genKeys;
--- src/lib/gen-keys.js
+++ src/lib/gen-keys.js
@@ -1,30 +1,24 @@
 const debug = require('debug')('rss-o-bot')
-const {Observable: O} = require('rx')
 
 /**
  * OS specific approach. Works on MacOS but probably not
  * on Windows. It probably also works on Linux. If this doesn't
  * work, the RSA keys must be generated manualy.
  */
-const genKeys = (H, Errors) => {
-  let i = 0
-  const genKeysBound = config => {
-    const privateKeyPath = H.privateKeyPath(config)
-    const publicKeyPath = H.publicKeyPath(config)
-    return (
-      H.exec('openssl genrsa 4096 -outform PEM')
-        .do(() => debug('Tried to generate RSA private key'))
-        .flatMap(H.isFile(privateKeyPath).catch(() => Errors.throwO(Errors.FAILED_TO_GEN_PRIV_KEY)))
-        .doOnError(err => console.log(err))
-        .flatMap(H.exec(`openssl rsa -pubout -in ${privateKeyPath}`))
-        .flatMap(k => console.log(`writting to ${privateKeyPath}: ${k}`) || H.writeFile(privateKeyPath, k))
-        .flatMap(k => H.writeFile(publicKeyPath, k))
-        .flatMap(H.isFile(privateKeyPath).catch(() => Errors.throwO(Errors.FAILED_TO_GEN_PUB_KEY)))
-        .do(() => debug('Generated keypair.'))
-        .catch(err => ++i > 1 ? O.throw(err) : genKeysBound(config))
-    )
-  }
-  return genKeysBound
+const genKeys = (H, Errors) => config => {
+  const privateKeyPath = H.privateKeyPath(config)
+  const publicKeyPath = H.publicKeyPath(config)
+  return (
+    H.exec('openssl genrsa 4096 -outform PEM')
+      .do(() => debug('Tried to generate RSA private key'))
+      .flatMap(k => H.writeFile(privateKeyPath, k))
+      .flatMap(H.isFile(privateKeyPath).catch(() => Errors.throwO(Errors.FAILED_TO_GEN_PRIV_KEY)))
+      .flatMap(H.exec(`openssl rsa -pubout -in ${privateKeyPath}`))
+      .flatMap(k => H.writeFile(publicKeyPath, k))
+      .flatMap(H.isFile(publicKeyPath).catch(() => Errors.throwO(Errors.FAILED_TO_GEN_PUB_KEY)))
+      .do(() => debug('Generated keypair.'))
+      .retry(2)
+  )
 }
 
 module.exports = genKeys
